4d76a20d6875a01cb5389be56f0d3256f3582f69
Nova Gh <nghimire.dev@gmail.com>
2021-06-28 11:23:42 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="bg">Hello World</h1>
+      <h1 className="text-4xl text-indigo-500">Hello World</h1>
     </div>
   );
 }
